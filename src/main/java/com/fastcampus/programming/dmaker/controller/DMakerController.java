f57b130314639770299ec7a633011fc27108a34d
Miyoung <rhaldud89@gmail.com>
2021-10-29 21:04:02 +0900

    Add Validation Check

--- src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -1,12 +1,15 @@
 package com.fastcampus.programming.dmaker.controller;
 
+import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.service.DMakerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -17,11 +20,22 @@ public class DMakerController {
 
     private final DMakerService dMakerService;
 
-    @GetMapping("/create-developer")
+    @GetMapping("/developers")
     public List<String> getAllDevelopers(){
         log.info("GET /create-developer HTTP/1.1");
 
-        dMakerService.createDeveloper();
+//        dMakerService.createDeveloper(request);
+
+        return List.of("Olaf");
+    }
+
+    @PostMapping("/create-developer")
+    public List<String> createDeveloper(@Valid @RequestBody CreateDeveloper.Request request)
+    {
+        log.info("request: {}", request);
+        log.info("POST /create-developer HTTP/1.1");
+
+        dMakerService.createDeveloper(request);
 
         return List.of("Olaf");
     }
